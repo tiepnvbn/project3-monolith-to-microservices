61751a9599361d0edc7c2c94f0f9a6895c096efe
tiepnvbn <tiepnvbn@gmail.com>
2022-08-03 18:03:23 +0700

    init project with structure

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Oneno23456789
+export POSTGRES_HOST=database-1.cb5nwyq1s9hf.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=my-511548998732-bucket-p3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
