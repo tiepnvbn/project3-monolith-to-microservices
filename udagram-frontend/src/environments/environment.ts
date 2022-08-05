51c10a949b5b742141d1e8187834332d504a09be
tiepnvbn <tiepnvbn@gmail.com>
2022-08-05 18:04:42 +0700

    update deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0877d6cb31d241aa8643238767849f3-2000557445.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
